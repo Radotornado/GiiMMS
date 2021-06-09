79ddaf0bad32602c3a186824ac7ff90ff35b09ad
Radotornado <radoslav.mandev99@gmail.com>
2021-06-09 13:05:59 +0200

    Initialize spring boot project.

---
+++ GiiMMS/src/main/java/de/uni_passau/fim/giimms/GiiMmsApplication.java
@@ -0,0 +1,13 @@
+package de.uni_passau.fim.giimms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiiMmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiiMmsApplication.class, args);
+    }
+
+}
---
+++ GiiMMS/src/test/java/de/uni_passau/fim/giimms/GiiMmsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.uni_passau.fim.giimms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiiMmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
