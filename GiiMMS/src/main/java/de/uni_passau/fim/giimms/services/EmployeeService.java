27666f6f7ac83069bb910ef304d12aadc4fee4a7
Radotornado <radoslav.mandev99@gmail.com>
2021-06-16 19:07:30 +0200

    Added spring security configuration.

--- GiiMMS/src/main/java/de/uni_passau/fim/giimms/services/EmployeeService.java
+++ GiiMMS/src/main/java/de/uni_passau/fim/giimms/services/EmployeeService.java
@@ -1,9 +1,10 @@
 package de.uni_passau.fim.giimms.services;
 
 import de.uni_passau.fim.giimms.util.Employee;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface EmployeeService {
+public interface EmployeeService extends UserDetailsService {
     Employee save(Employee employee);
 }
--- GiiMMS/src/main/java/de/uni_passau/fim/giimms/services/EmployeeServiceImpl.java
+++ GiiMMS/src/main/java/de/uni_passau/fim/giimms/services/EmployeeServiceImpl.java
@@ -4,6 +4,8 @@ import de.uni_passau.fim.giimms.repositories.EmployeeRepository;
 import de.uni_passau.fim.giimms.util.Employee;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,5 +18,10 @@ public class EmployeeServiceImpl implements EmployeeService{
     @Override
     public Employee save(Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null; // todo
     }
 }
