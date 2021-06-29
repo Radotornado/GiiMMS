d875686eaca7cfb8b576371008b56c8137df39e1
Radotornado <radoslav.mandev99@gmail.com>
2021-06-29 15:23:40 +0200

    Updates your real location if it is different from what is in the DB.

--- GiiMMS/src/main/resources/static/js/map.js
+++ GiiMMS/src/main/resources/static/js/map.js
@@ -1,13 +1,47 @@
+let map, infoWindow;
+
 function initMap() {
+    // get last known location from the employee object
     var text = document.getElementById("map");
     var coord  = text.textContent.split(", ");
     const pos = { lat: parseFloat(coord[0]), lng: parseFloat(coord[1]) };
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 17,
+    // set it as default (if unable to get current one)
+    map = new google.maps.Map(document.getElementById("map"), {
         center: pos,
+        zoom: 17,
     });
-    const marker = new google.maps.Marker({
-        position: pos,
-        map: map,
-    });
+    infoWindow = new google.maps.InfoWindow();
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                };
+                infoWindow.setPosition(pos);
+                infoWindow.setContent("Here you are.");
+                infoWindow.open(map);
+                map.setCenter(pos);
+            },
+            () => {
+                handleLocationError(true, infoWindow, map.getCenter());
+            }
+        );
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+    updateLocationObject(pos);
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+        browserHasGeolocation
+            ? "Error: The Geolocation service failed."
+            : "Error: Your browser doesn't support geolocation."
+    );
+    infoWindow.open(map);
 }
